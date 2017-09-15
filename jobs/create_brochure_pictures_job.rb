d9024b6870efb1be529773dedb40fe269b5fe549
hamityay <hamityay@hotmail.com>
2017-09-15 19:29:31 +0300

    changed variable for live.

--- jobs/create_brochure_pictures_job.rb
+++ jobs/create_brochure_pictures_job.rb
@@ -5,7 +5,8 @@ class CreateBrochurePicturesJob < ActiveJob::Base
     brochure = Brochure.find brochure_id
     pdf_file_path = brochure.file.path
     a = brochure.file.path.split '/'
-    pic_path = "/"+a[1]+"/"+a[2]+"/"+a[3]+"/"+a[4]+"/"+a[5]+"/"+a[6]+"/"+a[7]+"/"+a[8]+"/"+a[9]+"/"+a[10]+"/"+a[11]
+    # pic_path = "/"+a[1]+"/"+a[2]+"/"+a[3]+"/"+a[4]+"/"+a[5]+"/"+a[6]+"/"+a[7]+"/"+a[8]+"/"+a[9]+"/"+a[10]+"/"+a[11]
+    pic_path = "/"+a[1]+"/"+a[2]+"/"+a[3]+"/"+a[4]+"/"+a[5]+"/"+a[6]+"/"+a[7]+"/"+a[8]+"/"+a[9]
     brochure_dir = "#{Rails.root}/tmp/brochures"
     brochure_files_dir = "#{brochure_dir}/#{brochure.id}"
     # pdf_file_path = "#{brochure_files_dir}/#{self.id}.pdf"
